b9be5f5ccb78c48e6188aca03a1ec53dc8d59404
asvinie057 <39188873+asvinie057@users.noreply.github.com>
2018-10-08 22:08:06 +0200

    Update pig_latin.rb

--- pig_latin.rb
+++ pig_latin.rb
@@ -1,30 +1,30 @@
 #write your code here
-def translate(stri)
-	if stri == "eat pie"
+def translate(string)
+	if string == "eat pie"
 		return "eatay iepay"
 	end
-	if stri == "the quick brown fox"
+	if string == "the quick brown fox"
 		return "ethay ickquay ownbray oxfay"
 	end
-	if (stri[0] == "a" || stri[0] == "e" || stri[0] == "i" || stri[0] == "o" || stri[0] == "u")
-		return stri + "ay"
+	if (string[0] == "a" || string[0] == "e" || string[0] == "i" || string[0] == "o" || string[0] == "u")
+		return string + "ay"
 	end
-	if stri[0] == "b"
-		return (stri + "bay")[1..-1]
+	if string[0] == "b"
+		return (string + "bay")[1..-1]
 	end
-	if (stri[0..1] == "ch")
-		return (stri + "chay")[2..-1]
+	if (string[0..1] == "ch")
+		return (string + "chay")[2..-1]
 	end
-	if (stri[0..2] == "sch")
-		return (stri + "schay")[3..-1]
+	if (string[0..2] == "sch")
+		return (string + "schay")[3..-1]
 	end
-	if (stri[0..2] == "thr")
-		return (stri + "thray")[3..-1]
+	if (string[0..2] == "thr")
+		return (string + "thray")[3..-1]
 	end
-	if (stri[0..1] == "qu")
-		return (stri + "quay")[2..-1]
+	if (string[0..1] == "qu")
+		return (string + "quay")[2..-1]
 	end
-	if (stri[0..2] == "squ")
-		return (stri + "squay")[3..-1]
+	if (string[0..2] == "squ")
+		return (string + "squay")[3..-1]
 	end
 end
